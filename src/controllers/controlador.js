376c84cb0c26a0fe0851f2bbf2d8838406ab9e6e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-07-09 15:39:10 -0500

    inicio proyecto

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+function cambiarCaja(){
+    let boton=document.getElementById("botonChat")
+    let cajaChat=document.getElementById("cajaChat")
+
+    if(cajaChat.style.display=='none' || cajaChat.style.display==''){
+        cajaChat.style.display="block"
+    }else{
+        cajaChat.style.display="none"
+    }
+}    
+
